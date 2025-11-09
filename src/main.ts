c8e05994c4137be5132d7400ba36163f13566f10
makson10 <makson94021@gmail.com>
2025-11-10 00:50:56 +0200

    test deployment

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: process.env.CLIENT_ORIGIN });
+  app.enableCors({
+    origin: [process.env.CLIENT_ORIGIN],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 
